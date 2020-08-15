4eab2591c732eb21229ff49f2cd018a4c4d49e9b
ahmedabasery <engahmedabasery@yahoo.com>
2020-08-15 05:04:35 +0300

    redux devtools addition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import Reducers from "./reducers";
 import { Provider } from "react-redux";
 
-const store = createStore(Reducers);
+const composeEnahancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(Reducers, composeEnahancers(applyMiddleware()));
 
 ReactDom.render(
   <Provider store={store}>
